fb8b723c22f52fabef428ea90a1555df8c670ec0
Cevdet Karakulak <cevdetkarakulak_83@hotmail.com>
2025-03-08 18:05:41 +0300

    update

---
+++ Core_Proje/ViewComponents/Skill/SkillList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Skill
+{
+    public class SkillList : ViewComponent
+    {
+        SkillManager skillm = new SkillManager(new EfSkillDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = skillm.TGetList();
+            return View(values);
+        }
+    }
+}
