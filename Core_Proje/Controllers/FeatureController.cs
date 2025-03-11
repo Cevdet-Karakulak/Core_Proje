c088d7d8fe7b34c33495af9db9f25db9ef139760
Cevdet Karakulak <cevdetkarakulak_83@hotmail.com>
2025-03-11 23:22:54 +0300

    update

--- Core_Proje/Controllers/FeatureController.cs
+++ Core_Proje/Controllers/FeatureController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
 {
     public class FeatureController : Controller
     {
+        FeatureManager fm = new FeatureManager(new EfFeatureDal());
+        [HttpGet]
         public IActionResult Index()
+        {        
+            ViewBag.v1 = "Düzenleme";
+            ViewBag.v2 = "Öne Çıkanlar";
+            ViewBag.v3 = "Öne Çıkan Sayfası";
+            var values = fm.TGetById(1);
+            return View(values);
+        }       
+        [HttpPost]
+        public IActionResult Index(Feature p)
         {
-            return View();
+            fm.TUpdate(p);
+            return RedirectToAction("Index","Default");
         }
     }
 }
