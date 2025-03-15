e94d59dffc0ffb37a2ef008282f943a078db6820
Cevdet Karakulak <cevdetkarakulak_83@hotmail.com>
2025-03-15 23:41:54 +0300

    update

---
+++ Core_Proje/Areas/Writer/Models/UserRegisterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core_Proje.Areas.Writer.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage ="Lütfen Kullanıcı Adını Girin")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Lütfen Şifreyi Girin")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Lütfen Şifreyi Tekrar Girin")]
+        [Compare("Password",ErrorMessage ="Şiifrreler uyumlu değil!")]
+        public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "Lütfen Mail Adresini Girin")]
+        public string Mail { get; set; }
+    }
+}
--- DataAccessLayer/Migrations/20250315203947_mig_identitytest.Designer.cs
+++ DataAccessLayer/Migrations/20250315203947_mig_identitytest.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250315203947_mig_identitytest")]
+    partial class mig_identitytest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
